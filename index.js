c9692130c1cecb9a1d1770c49ee35980e7e201db
Tech Plus <techplus741gmail.com>
2025-07-22 05:35:38 +0100

    Done with the index page

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ let menu = document.getElementById("menu");
 let nav = document.getElementById("nav");
 let hamburg = document.getElementById("hamburg");
 let cancel = document.getElementById("cancel");
+let container = document.getElementById("container1")
 
 hamburg.addEventListener("click", function() {
     hamburg.style.display = "none";
     menu.style.display = "block"
+    container.style.display = "none";
 })
 
 cancel.addEventListener("click", function() {
     hamburg.style.display = "block";
     menu.style.display = "none";
+    container.style.display = "block";
 })
